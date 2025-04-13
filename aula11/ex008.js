db122aad0d6b87e67ef536048862a4ff1c66e792
Jordana Kicköfel <jordana.grutzmann@gmail.com>
2025-04-13 11:36:35 -0300

    Adicionando os Exercícios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 99
+console.log(`A velocidade do carro é de ${vel}km/h`)
+if(vel > 60){//exemplo de condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
